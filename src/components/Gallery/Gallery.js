2885b01c6e3cf72a27aa29648c83f115afec7299
Hakan <hakanozsoy99@gmail.com>
2025-03-17 14:10:05 +0300

    f

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -308,19 +308,30 @@ export default function Gallery({ images }) {
                         </div>
 
 
-                        <button type="button" className="gallery-prev" onClick={() => { handleImage(-1) }} onTouchStart={() => {
-                            console.log('naber :>> ', "naber");
-                            prev_next_show_attr("gallery-prev")
-                        }}
+                        <button type="button" className="gallery-prev" onClick={() => { handleImage(-1) }}
+                            onTouchStart={() => {
+                                console.log('naber :>> ', "naber");
+                                prev_next_show_attr("gallery-prev")
+                            }}
                             onTouchEnd={() => {
                                 console.log('naber :>> ', "naber");
                                 prev_next_hide_attr("gallery-prev")
-                            }}>
+                            }}
+                        >
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" className="bi bi-chevron-left" viewBox="0 0 16 16">
                                 <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0" />
                             </svg>
                         </button>
-                        <button type="button" className="gallery-next" onClick={() => { handleImage(1) }}>
+                        <button type="button" className="gallery-next" onClick={() => { handleImage(1) }}
+                            onTouchStart={() => {
+                                console.log('naber :>> ', "naber");
+                                prev_next_show_attr("gallery-next")
+                            }}
+                            onTouchEnd={() => {
+                                console.log('naber :>> ', "naber");
+                                prev_next_hide_attr("gallery-next")
+                            }}
+                        >
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" className="bi bi-chevron-right" viewBox="0 0 16 16">
                                 <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" />
                             </svg>
